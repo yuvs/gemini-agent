c68b1d044681c624172f7cfc31b0d19eeaaf2da8
yuvs <ideasandmethods@gmail.com>
2025-12-26 14:31:15 -0500

    refactored chat and added verbose flag

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
-import argparse 
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get('GEMINI_API_KEY')
-if api_key is None:
-    raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
-
-client = genai.Client(api_key=api_key)
 
 def main():
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    # Now we can access `args.user_prompt`
-    content = args.user_prompt
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=content)
-    
+        contents=messages,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
-    print(f"User prompt: {content}")
-
-    if response.usage_metadata is not None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise ValueError("Response usage metadata is None.")
-    
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
