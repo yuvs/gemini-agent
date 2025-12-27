c54f04fb894728f4506d5427aa155efe32ae4261
yuvs <ideasandmethods@gmail.com>
2025-12-27 01:08:04 -0500

    refactor get file content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,16 +5,17 @@ MAX_CHARS = 10000
 def get_file_content(working_directory, file_path):
     try:
         abs_working_dir = os.path.abspath(working_directory)
-        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
-        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
-            return f'Error: Cannot access "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(target_file):
-            return f'Error: "{file_path}" is not a valid file'
-        with open(target_file, "r") as f:
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(abs_file_path, "r") as f:
             content = f.read(MAX_CHARS)
-            # After reading the first MAX_CHARS...
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
         return content
     except Exception as e:
-        return f"Error reading file: {e}"
+        return f'Error reading file "{file_path}": {e}'
--- test_get_file_content.py
+++ test_get_file_content.py
@@ -2,31 +2,15 @@ from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_file_content("calculator", "lorem.txt")
-    print("Result for lorem.txt:")
-    print(result)
-    print("")
-"""
     result = get_file_content("calculator", "main.py")
-    print("Result for main.py:")
     print(result)
-    print("")
 
     result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for pkg/calculator.py:")
     print(result)
-    print("")
 
     result = get_file_content("calculator", "/bin/cat")
-    print("Result for /bin/cat:")
     print(result)
-    print("")
 
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for pkg/does_not_exist.py:")
-    print(result)
-    print("")
-"""
 
 if __name__ == "__main__":
     test()
